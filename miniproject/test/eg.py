b37382a67534f63421b9b91eb009afdaf95e725e
JustusKevinT <103323235+JustusKevinT@users.noreply.github.com>
2023-11-17 15:18:13 +0530

    Delete miniproject directory

--- miniproject/test/eg.py
+++
@@ -1,9 +0,0 @@
-lst = '127.0.0.1 www.javatpoint.com,127.0.0.1 www.facebook.com'
-lst_temp = lst.split(',')
-print(lst_temp)
-fp = open('test.txt','r+')
-lines = fp.readlines()
-print(lines)
-word = '127.0.0.1 www.facebook.com'
-if word in lst_temp:
-    print(word)
